361ce352363fea3f0162df26c1d0eb34e15cdbf2
isengrims <104489572+isengrims@users.noreply.github.com>
2022-06-21 19:08:02 +0400

    [TH2-3586] Draft dirty http conn.

---
+++ src/main/kotlin/com/exactpro/th2/util/CONSTANTS.kt
@@ -0,0 +1,14 @@
+package com.exactpro.th2.util
+
+const val HTTP_HEADER_VALUE_SEPARATOR = ","
+const val HTTP_CONTENT_TYPE_HEADER    = "Content-Type"
+const val HTTP_HEADER_SEPARATOR       = ":"
+const val STATUS_PROPERTY             = "status"
+const val URI_PROPERTY                = "uri"
+const val METHOD_PROPERTY             = "method"
+const val HTTP_AUTH_HEADER            = "Authorization"
+const val CONTENT_TYPE_PROPERTY       = "contentType"
+const val HTTP_LINE_END_INDICATOR     = "\n"
+const val HTTP_METHOD_HEAD            = "HEAD"
+const val HTTP_CONNECTION_HEADER      = "Connection"
+const val HTTP_CONNECTION_KEEP_ALIVE  = "Keep-Alive"
